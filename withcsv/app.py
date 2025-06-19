c6aefdf8e8c28d6fa0faaa150aceac815b97f982
Mekaela <mekaelas@hotmail.com>
2025-06-19 10:46:04 +0100

    Model working with app

--- withcsv/app.py
+++ withcsv/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,26 +13,36 @@ with open('./withcsv/soc_classifier_model.pkl', 'rb') as model_file:
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
-    # Get user input from the form
-    Tannual = float(request.form['Tannual'])
-    Pannual = float(request.form['Pannual'])
-    Tillage = str(request.form['Tillage'])
-    CoverCropGroup = str(request.form['CoverCropGroup'])
-    GrainCropGroup = str(request.form['GrainCropGroup'])
-    # Prepare the input for prediction
-    if Tillage == 'Yes':
-        Tillage = 1
-    if CoverCropGroup == 'Yes':
-        CoverCropGroup = 1
-    user_input = np.array([[Tannual, Pannual, Tillage, CoverCropGroup, GrainCropGroup]])
-    #  Make prediction
-    # need to make soc_predictor into a python file to call x_train and y_train to fit the model
-    model.fit(X_train, y_train)
-    # change this to user_input once it works
-    prediction = model.predict([22, 900, 1, 1, 0])
-    return f'The predicted value is: {prediction[0]}'
+    prediction = None
+    # user_input = {
+    #     'tannual': '',
+    #     'pannual': '',
+    #     'tillage': 'None',  # Default value
+    #     'cover_crop': 'None',  # Default value
+    #     'grain_crop': 'Monoculture'  # Default value
+    # }
+    if request.method == 'POST':
+        # Get user input from the form
+        Tannual = float(request.form['Tannual'])
+        Pannual = float(request.form['Pannual'])
+        Tillage = str(request.form['Tillage'])
+        CoverCropGroup = str(request.form['CoverCropGroup'])
+        GrainCropGroup = str(request.form['GrainCropGroup'])
+
+        user_input = pd.DataFrame({
+            'Tannual': [Tannual],
+            'Pannual': [Pannual],
+            'Tillage': [Tillage],
+            'CoverCropGroup': [CoverCropGroup],
+            'GrainCropGroup': [GrainCropGroup]
+        })
+
+        #  Make prediction
+        prediction = model.predict(user_input)
+
+    return render_template('prediction.html', prediction=prediction[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
